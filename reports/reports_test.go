4a17c4e7c0e2d51b17c2840c26141f3f62629bbf
it_akumi <it.akumi1011@gmail.com>
2019-07-09 22:07:45 +0900

    Add tests for GetSummary and GetWeekly

--- reports/reports_test.go
+++ reports/reports_test.go
@@ -28,22 +28,6 @@ func TestNewClient(t *testing.T) {
 	}
 }
 
-const (
-	apiToken    string = "api_token"
-	userAgent   string = "user_agent"
-	workSpaceId string = "workspace_id"
-)
-
-type detailedReport struct {
-	TotalCount int `json:"total_count"`
-	PerPage    int `json:"per_page"`
-	Data       []struct {
-		User        string `json:"user"`
-		Project     string `json:"project"`
-		Description string `json:"description"`
-	} `json:"data"`
-}
-
 func setupMockServer_200_Ok(t *testing.T, testdataFilePath string) (*httptest.Server, []byte) {
 	testdata, err := ioutil.ReadFile(testdataFilePath)
 	if err != nil {
@@ -70,6 +54,22 @@ func setupMockServer_401_Unauthorized(t *testing.T) (*httptest.Server, []byte) {
 	}))
 
 	return mockServer, errorTestData
+}
+
+const (
+	apiToken    string = "api_token"
+	userAgent   string = "user_agent"
+	workSpaceId string = "workspace_id"
+)
+
+type detailedReport struct {
+	TotalCount int `json:"total_count"`
+	PerPage    int `json:"per_page"`
+	Data       []struct {
+		User        string `json:"user"`
+		Project     string `json:"project"`
+		Description string `json:"description"`
+	} `json:"data"`
 }
 
 func TestGetDetailed_200_Ok(t *testing.T) {
@@ -118,5 +118,143 @@ func TestGetDetailed_401_Unauthorized(t *testing.T) {
 	}
 	if !reflect.DeepEqual(actualReportsError, expectedReportsError) {
 		t.Error("GetDetailed fails to decode ReportsError though it returns error as expected")
+	}
+}
+
+type summaryReport struct {
+	Data []struct {
+		Id    int `json:"id"`
+		Title struct {
+			Project string `json:"project"`
+			Color   string `json:"color"`
+			User    string `json:"user"`
+		} `json:"title"`
+		Time  int `json:"time"`
+		Items []struct {
+			Title struct {
+				Project   string `json:"project"`
+				User      string `json:"user"`
+				TimeEntry string `json:"time_entry"`
+			} `json:"title"`
+			Time int `json:"time"`
+		} `json:"items"`
+	} `json:"data"`
+}
+
+func TestGetSummary_200_Ok(t *testing.T) {
+	mockServer, summaryTestData := setupMockServer_200_Ok(t, "testdata/summary.json")
+	defer mockServer.Close()
+
+	actualSummaryReport := new(summaryReport)
+	client := NewClient(apiToken, baseURL(mockServer.URL))
+	err := client.GetSummary(&SummaryRequestParameters{
+		StandardRequestParameters: &StandardRequestParameters{
+			UserAgent:   userAgent,
+			WorkSpaceId: workSpaceId,
+		},
+	}, actualSummaryReport)
+	if err != nil {
+		t.Error("GetSummary returns error though it gets '200 OK'")
+	}
+
+	expectedSummaryReport := new(summaryReport)
+	if err := json.Unmarshal(summaryTestData, expectedSummaryReport); err != nil {
+		t.Error(err.Error())
+	}
+	if !reflect.DeepEqual(actualSummaryReport, expectedSummaryReport) {
+		t.Error("GetSummary fails to decode summaryReport")
+	}
+}
+
+func TestGetSummary_401_Unauthorized(t *testing.T) {
+	mockServer, unauthorizedTestData := setupMockServer_401_Unauthorized(t)
+	defer mockServer.Close()
+
+	client := NewClient(apiToken, baseURL(mockServer.URL))
+	actualReportsError := client.GetSummary(&SummaryRequestParameters{
+		StandardRequestParameters: &StandardRequestParameters{
+			UserAgent:   userAgent,
+			WorkSpaceId: workSpaceId,
+		},
+	}, new(summaryReport))
+	if actualReportsError == nil {
+		t.Error("GetSummary doesn't return error though it gets '401 Unauthorized'")
+	}
+
+	expectedReportsError := new(ReportsError)
+	if err := json.Unmarshal(unauthorizedTestData, expectedReportsError); err != nil {
+		t.Error(err.Error())
+	}
+	if !reflect.DeepEqual(actualReportsError, expectedReportsError) {
+		t.Error("GetSummary fails to decode ReportsError though it returns error as expected")
+	}
+}
+
+type weeklyReport struct {
+	WeekTotals []interface{} `json:"week_totals"`
+	Data       []struct {
+		Title struct {
+			Project string `json:"project"`
+			Color   string `json:"color"`
+			User    string `json:"user"`
+		} `json:"title"`
+		Totals  []interface{} `json:"totals`
+		Details []struct {
+			Title struct {
+				Project string `json:"project"`
+				Color   string `json:"color"`
+				User    string `json:"user"`
+			} `json:"title"`
+			Totals []interface{} `json:"totals`
+		} `json:"details"`
+	} `json:"data"`
+}
+
+func TestGetWeekly_200_Ok(t *testing.T) {
+	mockServer, weeklyTestData := setupMockServer_200_Ok(t, "testdata/weekly.json")
+	defer mockServer.Close()
+
+	actualWeeklyReport := new(weeklyReport)
+	client := NewClient(apiToken, baseURL(mockServer.URL))
+	err := client.GetWeekly(&WeeklyRequestParameters{
+		StandardRequestParameters: &StandardRequestParameters{
+			UserAgent:   userAgent,
+			WorkSpaceId: workSpaceId,
+		},
+	}, actualWeeklyReport)
+	if err != nil {
+		t.Error("GetWeekly returns error though it gets '200 OK'")
+	}
+
+	expectedWeeklyReport := new(weeklyReport)
+	if err := json.Unmarshal(weeklyTestData, expectedWeeklyReport); err != nil {
+		t.Error(err.Error())
+	}
+	if !reflect.DeepEqual(actualWeeklyReport, expectedWeeklyReport) {
+		t.Error("GetWeekly fails to decode weeklyReport")
+	}
+}
+
+func TestGetWeekly_401_Unauthorized(t *testing.T) {
+	mockServer, unauthorizedTestData := setupMockServer_401_Unauthorized(t)
+	defer mockServer.Close()
+
+	client := NewClient(apiToken, baseURL(mockServer.URL))
+	actualReportsError := client.GetWeekly(&WeeklyRequestParameters{
+		StandardRequestParameters: &StandardRequestParameters{
+			UserAgent:   userAgent,
+			WorkSpaceId: workSpaceId,
+		},
+	}, new(weeklyReport))
+	if actualReportsError == nil {
+		t.Error("GetWeekly doesn't return error though it gets '401 Unauthorized'")
+	}
+
+	expectedReportsError := new(ReportsError)
+	if err := json.Unmarshal(unauthorizedTestData, expectedReportsError); err != nil {
+		t.Error(err.Error())
+	}
+	if !reflect.DeepEqual(actualReportsError, expectedReportsError) {
+		t.Error("GetWeekly fails to decode ReportsError though it returns error as expected")
 	}
 }
