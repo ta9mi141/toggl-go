d02cbf89e8b86198153a452d4544e7d9dde0bf38
it_akumi <it.akumi1011@gmail.com>
2019-09-30 23:39:07 +0900

    Add an example of package "reports"

---
+++ reports/example_test.go
@@ -0,0 +1,51 @@
+package reports_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/it-akumi/toggl-go/reports"
+)
+
+// Users must define a data structure for each report
+type summary struct {
+	Data []struct {
+		Title struct {
+			Project string
+		}
+		Items []struct {
+			Title struct {
+				TimeEntry string `json:time_entry`
+			}
+			Time int
+		}
+	}
+}
+
+func Example() {
+	client := reports.NewClient("YOUR_API_TOKEN")
+	summaryReport := new(summary)
+	err := client.GetSummary(
+		context.Background(),
+		&reports.SummaryRequestParameters{
+			StandardRequestParameters: &reports.StandardRequestParameters{
+				UserAgent:   "YOUR_USER_AGENT",
+				WorkSpaceId: "YOUR_WORKSPACE_ID",
+				Since:       time.Now().AddDate(0, 0, -7),
+				Until:       time.Now(),
+			},
+		},
+		summaryReport,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, datum := range summaryReport.Data {
+		fmt.Println(datum.Title.Project)
+		for _, item := range datum.Items {
+			fmt.Printf("Time entry: %v, Time: %d hours", item.Title.TimeEntry, item.Time/1000/3600)
+		}
+	}
+}
