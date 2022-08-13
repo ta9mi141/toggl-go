9da10aeb876e0d6cb35ea2d80895d2631c1dbc65
Takumi Ishii <ta9mi141@gmail.com>
2022-08-13 20:27:03 +0900

    Use Ptr to get a pointer to any value

--- track/types.go
+++ track/types.go
@@ -1,28 +1,6 @@
 package track
 
-import "time"
-
-// Int returns a pointer to the int value.
-func Int(v int) *int {
-	return &v
-}
-
-// Float64 returns a pointer to the float64 value.
-func Float64(v float64) *float64 {
-	return &v
-}
-
-// String returns a pointer to the string value.
-func String(v string) *string {
-	return &v
-}
-
-// Bool returns a pointer to the bool value.
-func Bool(v bool) *bool {
-	return &v
-}
-
-// Time returns a pointer to the time.Time value.
-func Time(v time.Time) *time.Time {
+// Ptr returns a pointer to the given value.
+func Ptr[T any](v T) *T {
 	return &v
 }
