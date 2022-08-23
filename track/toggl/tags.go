ca46a80ba9e2ccd2122ec4c37cd923069abd724c
Takumi Ishii <ta9mi141@gmail.com>
2022-08-23 21:36:52 +0900

    Add tests for GetTags

--- track/toggl/tags.go
+++ track/toggl/tags.go
@@ -1,6 +1,9 @@
 package toggl
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 // Tag represents the properties of a tag.
 type Tag struct {
@@ -9,4 +12,9 @@ type Tag struct {
 	Name        *string    `json:"name,omitempty"`
 	At          *time.Time `json:"at,omitempty"`
 	DeletedAt   *time.Time `json:"deleted_at,omitempty"`
+}
+
+// GetTags lists workspace tags.
+func (c *Client) GetTags(ctx context.Context, workspaceID int) ([]*Tag, error) {
+	return nil, nil
 }
---
+++ track/toggl/tags_test.go
@@ -0,0 +1,153 @@
+package toggl
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"path"
+	"reflect"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/ta9mi141/toggl-go/track"
+	"github.com/ta9mi141/toggl-go/track/internal"
+)
+
+func TestGetTags(t *testing.T) {
+	tests := []struct {
+		name string
+		in   struct {
+			statusCode   int
+			testdataFile string
+		}
+		out struct {
+			tags []*Tag
+			err  error
+		}
+	}{
+		{
+			name: "200 OK",
+			in: struct {
+				statusCode   int
+				testdataFile string
+			}{
+				statusCode:   http.StatusOK,
+				testdataFile: "testdata/tags/get_tags_200_ok.json",
+			},
+			out: struct {
+				tags []*Tag
+				err  error
+			}{
+				tags: []*Tag{
+					{
+						ID:          track.Ptr(12345678),
+						WorkspaceID: track.Ptr(1234567),
+						Name:        track.Ptr("toggl-go"),
+						At:          track.Ptr(time.Date(2020, time.January, 2, 3, 4, 5, 678901000, time.UTC)),
+					},
+				},
+				err: nil,
+			},
+		},
+		{
+			name: "400 Bad Request",
+			in: struct {
+				statusCode   int
+				testdataFile string
+			}{
+				statusCode:   http.StatusBadRequest,
+				testdataFile: "testdata/tags/get_tags_400_bad_request.json",
+			},
+			out: struct {
+				tags []*Tag
+				err  error
+			}{
+				tags: nil,
+				err: &internal.ErrorResponse{
+					StatusCode: 400,
+					Message:    "\"Missing or invalid workspace_id\"\n",
+					Header: http.Header{
+						"Content-Length": []string{"34"},
+						"Content-Type":   []string{"application/json; charset=utf-8"},
+						"Date":           []string{time.Now().In(time.FixedZone("GMT", 0)).Format(time.RFC1123)},
+					},
+				},
+			},
+		},
+		{
+			name: "403 Forbidden",
+			in: struct {
+				statusCode   int
+				testdataFile string
+			}{
+				statusCode:   http.StatusForbidden,
+				testdataFile: "testdata/tags/get_tags_403_forbidden",
+			},
+			out: struct {
+				tags []*Tag
+				err  error
+			}{
+				tags: nil,
+				err: &internal.ErrorResponse{
+					StatusCode: 403,
+					Message:    "",
+					Header: http.Header{
+						"Content-Length": []string{"0"},
+						"Date":           []string{time.Now().In(time.FixedZone("GMT", 0)).Format(time.RFC1123)},
+					},
+				},
+			},
+		},
+		{
+			name: "500 Internal Server Error",
+			in: struct {
+				statusCode   int
+				testdataFile string
+			}{
+				statusCode:   http.StatusInternalServerError,
+				testdataFile: "testdata/tags/get_tags_500_internal_server_error",
+			},
+			out: struct {
+				tags []*Tag
+				err  error
+			}{
+				tags: nil,
+				err: &internal.ErrorResponse{
+					StatusCode: 500,
+					Message:    "",
+					Header: http.Header{
+						"Content-Length": []string{"0"},
+						"Date":           []string{time.Now().In(time.FixedZone("GMT", 0)).Format(time.RFC1123)},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			workspaceID := 1234567
+			apiSpecificPath := path.Join(workspacesPath, strconv.Itoa(workspaceID), "tags")
+			mockServer := internal.NewMockServer(t, apiSpecificPath, tt.in.statusCode, tt.in.testdataFile)
+			defer mockServer.Close()
+
+			client := NewClient(WithAPIToken(internal.APIToken), withBaseURL(mockServer.URL))
+			tags, err := client.GetTags(context.Background(), workspaceID)
+
+			if !reflect.DeepEqual(tags, tt.out.tags) {
+				internal.Errorf(t, tags, tt.out.tags)
+			}
+
+			errorResp := new(internal.ErrorResponse)
+			if errors.As(err, &errorResp) {
+				if !reflect.DeepEqual(errorResp, tt.out.err) {
+					internal.Errorf(t, errorResp, tt.out.err)
+				}
+			} else {
+				if !reflect.DeepEqual(err, tt.out.err) {
+					internal.Errorf(t, err, tt.out.err)
+				}
+			}
+		})
+	}
+}
