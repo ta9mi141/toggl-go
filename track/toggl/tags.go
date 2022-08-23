359d3339475f2b49cc337fea6ebeb33d3a0ba49a
Takumi Ishii <ta9mi141@gmail.com>
2022-08-23 21:38:51 +0900

    Implement GetTags

--- track/toggl/tags.go
+++ track/toggl/tags.go
@@ -2,7 +2,11 @@ package toggl
 
 import (
 	"context"
+	"path"
+	"strconv"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 // Tag represents the properties of a tag.
@@ -16,5 +20,10 @@ type Tag struct {
 
 // GetTags lists workspace tags.
 func (c *Client) GetTags(ctx context.Context, workspaceID int) ([]*Tag, error) {
-	return nil, nil
+	var tags []*Tag
+	apiSpecificPath := path.Join(workspacesPath, strconv.Itoa(workspaceID), "tags")
+	if err := c.httpGet(ctx, apiSpecificPath, nil, &tags); err != nil {
+		return nil, errors.Wrap(err, "")
+	}
+	return tags, nil
 }
